220bd902d9d2cc53cdec389c92691dbe03c1acdb
expo2025-auto <danshicook@gmail.com>
2025-09-30 16:33:58 +0900

    Update expo2025-reserver.user.js

--- expo2025-reserver.user.js
+++ expo2025-reserver.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Expo2025 来場予約
 // @namespace    http://tampermonkey.net/
-// @version      2.4
+// @version      2.55
 // @author       You
 // @match        https://ticket.expo2025.or.jp/*
 // @run-at       document-idle
